fcd1da2f551f63d08451d0d51d4f50db3c8697f4
Victorola <victoluolatunji@gmail.com>
2024-11-03 23:29:59 +0100

    [fix build errors]

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import rootReducer from "./reducers";
 import rootSaga from "./sagas";
+import rootReducer from "./reducers";
+import createSagaMiddleware from "redux-saga";
+import { createStore, applyMiddleware } from "redux";
 
 const sagaMiddleware = createSagaMiddleware();
 
--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,5 +1,5 @@
-import { all } from "redux-saga/effects";
 import watchAuth from "./authSaga";
+import { all } from "redux-saga/effects";
 
 export default function* rootSaga() {
   yield all([watchAuth()]);
